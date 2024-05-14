da4015156efea587177e3f46f34eab84db24868e
srinathvv-dev <srinathvv.dev@gmail.com>
2024-05-15 00:07:25 +0530

    finalpush

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,104 @@
 import Link from "next/link";
 import styled from "styled-components";
 import Center from "@/components/Center";
-import {useContext, useState} from "react";
-import {CartContext} from "@/components/CartContext";
+import { useContext, useState } from "react";
+import { CartContext } from "@/components/CartContext";
 import BarsIcon from "@/components/icons/Bars";
 
 const StyledHeader = styled.header`
   background-color: #222;
+  overflow-x: hidden;
 `;
-const Logo = styled(Link)`
-  color:#fff;
-  text-decoration:none;
-  position: relative;
-  z-index: 3;
-`;
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 20px 0;
-`;
-const StyledNav = styled.nav`
-  ${props => props.mobileNavActive ? `
+
+const NavIcon = styled.button`
+  background: transparent;
+  border: none;
+  color: white;
+  font-size: 1.5rem;
+  cursor: pointer;
+  display: none;
+
+  @media (max-width: 750px) {
     display: block;
-  ` : `
+  }
+`;
+
+const Nav = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 3%;
+  padding-bottom: 35px;
+`;
+
+const NavBar = styled.div`
+  font-family: 'Poppins', sans-serif;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  width: 70%; /* Adjusted width */
+  height: 90px;
+  border-radius: 20px;
+  border: 2px solid white;
+    box-shadow: 12px 12px white;
+  padding: 0 15%;
+  margin-left:-3%;
+  
+`;
+
+const Logo = styled.h1`
+  font-family: 'Poppins', sans-serif;
+  font-size: 180%;
+  font-weight: 600;
+  color: white; /* Changed color to white */
+`;
+
+const NavLinksContainer = styled.div`
+  margin-top: 1%;
+  display: flex;
+  flex-direction: row;
+  gap: 8%;
+  align-items: center;
+  justify-content: center;
+`;
+
+const NavLink = styled(Link)`
+  font-size: 100%;
+  text-decoration: none;
+  color: white; /* Changed color to white */
+`;
+
+const NavButton = styled.button`
+  padding: 2% 4%;
+  font-size: 100%;
+  font-weight: 500;
+  width: 130px;
+  height: 40px;
+  border-radius: 8px;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  background-color: black;
+  transition: 600ms;
+
+  &:hover {
+    background-color: #FAF9F5;
+    color: black;
+    transform: translateX(-6px);
+    box-shadow: 7px 5px;
+    border: 2px solid rgb(63, 62, 62);
+  }
+`;
+
+const StyledNav = styled.nav`
+  ${(props) =>
+    props.mobileNavActive
+      ? `
+    display: block;
+  `
+      : `
     display: none;
   `}
   gap: 15px;
@@ -33,54 +109,38 @@ const StyledNav = styled.nav`
   right: 0;
   padding: 70px 20px 20px;
   background-color: #222;
+
   @media screen and (min-width: 768px) {
     display: flex;
     position: static;
     padding: 0;
   }
 `;
-const NavLink = styled(Link)`
-  display: block;
-  color:#aaa;
-  text-decoration:none;
-  padding: 10px 0;
-  @media screen and (min-width: 768px) {
-    padding:0;
-  }
-`;
-const NavButton = styled.button`
-  background-color: transparent;
-  width: 30px;
-  height: 30px;
-  border:0;
-  color: white;
-  cursor: pointer;
-  position: relative;
-  z-index: 3;
-  @media screen and (min-width: 768px) {
-    display: none;
-  }
-`;
 
 export default function Header() {
-  const {cartProducts} = useContext(CartContext);
-  const [mobileNavActive,setMobileNavActive] = useState(false);
+  const { cartProducts } = useContext(CartContext);
+  const [mobileNavActive, setMobileNavActive] = useState(false);
+
   return (
     <StyledHeader>
       <Center>
-        <Wrapper>
-          <Logo href={'/'}>1nlyCoffee</Logo>
-          <StyledNav mobileNavActive={mobileNavActive}>
-            <NavLink href={'/'}>Home</NavLink>
-            <NavLink href={'/products'}>All products</NavLink>
-            {/* <NavLink href={'/categories'}>Categories</NavLink> */}
-            {/* <NavLink href={'/account'}>Account</NavLink> */}
-            <NavLink href={'/cart'}>Cart ({cartProducts.length})</NavLink>
-          </StyledNav>
-          <NavButton onClick={() => setMobileNavActive(prev => !prev)}>
-            <BarsIcon />
-          </NavButton>
-        </Wrapper>
+        <Nav>
+          <NavBar>
+            <Logo>Portfolio</Logo>
+            <NavIcon onClick={() => setMobileNavActive((prev) => !prev)}>
+              <BarsIcon />
+            </NavIcon>
+            <StyledNav mobileNavActive={mobileNavActive}>
+              <NavLinksContainer>
+                <NavLink href="/">Home</NavLink>
+                <NavLink href="/about">About</NavLink>
+                <NavLink href="/works">Works</NavLink>
+                <NavLink href="/services">Services</NavLink>
+                <NavLink href="/contact">Contact</NavLink>
+              </NavLinksContainer>
+            </StyledNav>
+          </NavBar>
+        </Nav>
       </Center>
     </StyledHeader>
   );
---
+++ components/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Image from 'next/image';
+import Pg from './page.module.css';
+import Logo from '../components/icons/coffee.png';
+
+function Home() {
+  return (
+    <div>
+      <div className={Pg.maincont}>
+        <div className={Pg.cont}>
+          <div className={Pg.cont1}>
+            <h2 style={{ "--i": 2 }}>Welcome to </h2>
+            <h1 style={{ "--i": 3 }}>1ly Coffee</h1>
+            <p style={{ "--i": 4 }}>
+              Founder of Caliber Tech, freelancing fullstack developer in MERN stack, frontend developer in NEXT JS and also a videographer
+            </p>
+          </div>
+        </div>
+        <div className={Pg.img}>
+          <Image
+            style={{ "--i": 1 }}
+            className={Pg.image1}
+            src={Logo}
+            alt="logo"
+            width={500}
+            height={500}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
